159861c7dee4b005bbcd1a4c2991c7ffa74f3232
Steven A. Zahm <shazahm1@hotmail.com>
2014-05-27 13:10:37 -0400

    Cleanup disable core updates action/filters.

--- Function.php
+++ Function.php
@@ -221,6 +221,17 @@ class Disable_Updates {
 
 					// Disable Core Updates Only
 					remove_action( 'load-update-core.php', 'wp_update_core' );
+
+					# 2.3 to 2.7:
+					add_action( 'init', create_function( '', 'remove_action( \'init\', \'wp_version_check\' );' ), 2 );
+					add_filter( 'pre_option_update_core', '__return_null' );
+
+					# 2.8 to 3.0:
+					remove_action( 'wp_version_check', 'wp_version_check' );
+					remove_action( 'admin_init', '_maybe_update_core' );
+					add_filter( 'pre_transient_update_core', array( __CLASS__,'last_checked' ) );
+
+					# >3.0:
 					add_filter( 'pre_site_transient_update_core', array( __CLASS__,'last_checked' ) );
 
 					// Hide Update Notices in Admin Dashboard
@@ -259,15 +270,6 @@ class Disable_Updates {
 						wp_clear_scheduled_hook( 'wp_version_check' );
 
 					}
-
-					// Remove Updates Again (different method)
-
-					add_action( 'init', create_function( '$a', "remove_action( 'init', 'wp_version_check' );" ), 2 );
-					add_filter( 'pre_option_update_core', '__return_null' );
-
-					remove_action( 'wp_version_check', 'wp_version_check' );
-					remove_action( 'admin_init', '_maybe_update_core' );
-					add_filter( 'pre_transient_update_core', '__return_null' );
 
 					remove_action( 'load-themes.php', 'wp_update_themes' );
 					remove_action( 'load-update.php', 'wp_update_themes' );
