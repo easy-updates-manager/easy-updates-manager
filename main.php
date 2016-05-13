fdfba6479e4e6db02ec0f005622c868051f2763a
Ronald Huereca <ronalfy@gmail.com>
2016-05-13 18:40:40 -0500

    Bumping version number because I can.

--- main.php
+++ main.php
@@ -5,7 +5,7 @@ Plugin URI: https://wordpress.org/plugins/stops-core-theme-and-plugin-updates/
 Description: Manage and disable WordPress updates, including core, plugin, theme, and automatic updates - Works with Multisite and has built-in logging features.
 Author: Easy Updates Manager Team
 Version: 6.0.0
-Requires at least: 4.3
+Requires at least: 4.4
 Author URI: https://wordpress.org/plugins/stops-core-theme-and-plugin-updates/
 Contributors: kidsguide, ronalfy
 Text Domain: stops-core-theme-and-plugin-updates
